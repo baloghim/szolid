241e44c6f1f11c46f4c0b5c99419cdf475ae8d51
PetrovicsIstvan <103250640+PetrovicsIstvan@users.noreply.github.com>
2022-04-13 09:20:57 +0200

    :d

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {   
             Console.WriteLine("PetrovicsIstvan A Gigantikus");
-            Console.WriteLine("Egervári Martin");
+            Console.WriteLine("Egervári Martin A FeketeEmber");
             Console.ReadKey();
 
         }
