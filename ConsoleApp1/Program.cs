336b41ae1ed3c3c4f0fe3a4eb04ca01de1eb989b
baloghim <balogh.imre.mt@gmail.com>
2022-04-13 09:25:49 +0200

    q

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,11 +10,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {   
-            Console.WriteLine("PetrovicsIstvan A Gigantikusan kicsi a pömpölője");
-            Console.WriteLine("Egervári Martin A FeketeEmber");
-            
-            Console.WriteLine("Jonai Adam A Repper");
->>>>>>> 659ddac4444f44a3a44a3bb5106369d6b73d4079
+          
+
             Console.ReadKey();
 
         }
